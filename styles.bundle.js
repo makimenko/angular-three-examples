8f56ef148f742a235bea48bc4e9cc992b322b016
makimenko <mihails.akimenko@gmail.com>
2017-08-24 19:10:02 +0300

    Auto-generated commit

--- styles.bundle.js
+++ styles.bundle.js
@@ -35,7 +35,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 
 
 // module
-exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\n", ""]);
+exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\r\n", ""]);
 
 // exports
 

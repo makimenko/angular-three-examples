295f0061f1c067a0e06cdc1c34d268f454bd8173
Mihail Akimenko <mihails.akimenko@gmail.com>
2017-08-24 11:05:26 +0300

    empty three.js scene as Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SceneComponent } from "./scene/scene.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SceneComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/scene/scene.component.ts
@@ -0,0 +1,131 @@
+import { AfterViewInit, Component, ElementRef, Input, ViewChild, HostListener } from '@angular/core';
+import * as THREE from 'three';
+
+@Component({
+    selector: 'scene',
+    templateUrl: './scene.component.html',
+    styleUrls: ['./scene.component.css']
+})
+export class SceneComponent implements AfterViewInit {
+
+    private renderer: THREE.WebGLRenderer;
+    private camera: THREE.PerspectiveCamera;
+    private cameraTarget: THREE.Vector3;
+    public scene: THREE.Scene;
+
+    public fieldOfView: number = 60;
+    public nearClippingPane: number = 1;
+    public farClippingPane: number = 1100;
+
+    @ViewChild('canvas')
+    private canvasRef: ElementRef;
+
+    constructor() {
+
+    }
+
+    private get canvas(): HTMLCanvasElement {
+        return this.canvasRef.nativeElement;
+    }
+
+    private createScene() {
+        this.scene = new THREE.Scene();
+        this.scene.add(new THREE.AxisHelper(200));
+    }
+
+    private createLight() {
+        var light = new THREE.PointLight(0xffffff, 1, 1000);
+        light.position.set(0, 0, 100);
+        this.scene.add(light);
+
+        var light = new THREE.PointLight(0xffffff, 1, 1000);
+        light.position.set(0, 0, -100);
+        this.scene.add(light);
+    }
+
+    private createCamera() {
+        let aspectRatio = this.getAspectRatio();
+        this.camera = new THREE.PerspectiveCamera(
+            this.fieldOfView,
+            aspectRatio,
+            this.nearClippingPane,
+            this.farClippingPane
+        );
+
+        // Set position and look at
+        this.camera.position.x = 0;
+        this.camera.position.y = 0;
+        this.camera.position.z = 100;
+    }
+
+    private getAspectRatio(): number {
+        let height = this.canvas.clientHeight;
+        if (height === 0) {
+            return 0;
+        }
+        return this.canvas.clientWidth / this.canvas.clientHeight;
+    }
+
+    private startRendering() {
+        this.renderer = new THREE.WebGLRenderer({
+            canvas: this.canvas,
+            antialias: true
+        });
+        this.renderer.setPixelRatio(devicePixelRatio);
+        this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        this.renderer.setClearColor(0xffffff, 1);
+        this.renderer.autoClear = true;
+
+        let component: SceneComponent = this;
+
+        (function render() {
+            //requestAnimationFrame(render);
+            component.render();
+        }());
+    }
+
+    public render() {
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    /* EVENTS */
+
+    public onMouseDown(event: MouseEvent) {
+        console.log("onMouseDown");
+        event.preventDefault();
+    }
+
+    public onMouseUp(event: MouseEvent) {
+        console.log("onMouseUp");
+    }
+
+
+    @HostListener('window:resize', ['$event'])
+    public onResize(event: Event) {
+        this.canvas.style.width = "100%";
+        this.canvas.style.height = "100%";
+        console.log("onResize: " + this.canvas.clientWidth + ", " + this.canvas.clientHeight);
+
+        this.camera.aspect = this.getAspectRatio();
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+        this.render();
+    }
+
+    @HostListener('document:keypress', ['$event'])
+    public onKeyPress(event: KeyboardEvent) {
+        console.log("onKeyPress: " + event.key);
+    }
+
+    /* LIFECYCLE */
+    ngAfterViewInit() {
+        this.createScene();
+        this.createLight();
+        this.createCamera();
+        this.startRendering();
+    }
+
+}
